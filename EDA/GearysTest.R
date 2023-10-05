04848ceec7f40a0938b177681acf4fc1d5ce61b5
FayFrost107 <fayfrost107@gmail.com>
2023-10-05 12:47:58 +0100

    Code for Geary's Test

---
+++ EDA/GearysTest.R
@@ -0,0 +1,51 @@
+## Geary's test (mean* sqrt(n) )/ SD
+
+warmdata <- read.csv("Survival project effect size reftemp FF warm.csv")
+
+
+# Create a list of column names for each treatment
+column_sets <- list(warmdata[c("Temp1", "Mean1", "SD1", "N1", "ani_1")],
+                    warmdata[c("Temp2", "Mean2", "SD2", "N2", "ani_2")],
+                    warmdata[c("Temp3", "Mean3", "SD3", "N3", "ani_3")],
+                    warmdata[c("Temp4", "Mean4", "SD4", "N4", "ani_4")],
+                    warmdata[c("Temp5", "Mean5", "SD5", "N5", "ani_5")],
+                    warmdata[c("Temp6", "Mean6", "SD6", "N6", "ani_6")],
+                    warmdata[c("Temp7", "Mean7", "SD7", "N7", "ani_7")],
+                    warmdata[c("Temp8", "Mean8", "SD8", "N8", "ani_8")],
+                    warmdata[c("Temp9", "Mean9", "SD9", "N9", "ani_9")])
+
+
+#Gearys <- NA
+
+calculate_g <- function(temp, mean, sd, n){
+  if (is.na(temp)){
+    g <- NA  # Initialize as NA (numeric)
+  } else {
+    g <- (mean*sqrt(n))/sd
+  }
+  
+  return(g)
+}
+
+
+gdata <- c()
+for(i in 1:nrow(warmdata)){
+  row <- calculate_g(column_sets[[1]]$Temp1[i], column_sets[[1]]$Mean1[i], column_sets[[1]]$SD1[i], column_sets[[1]]$N1[i])
+  row <- cbind(row, calculate_g(column_sets[[2]]$Temp2[i], column_sets[[2]]$Mean2[i], column_sets[[2]]$SD2[i], column_sets[[2]]$N2[i]))
+  row <- cbind(row, calculate_g(column_sets[[3]]$Temp3[i], column_sets[[3]]$Mean3[i], column_sets[[3]]$SD3[i], column_sets[[3]]$N3[i]))
+  row <- cbind(row, calculate_g(column_sets[[4]]$Temp4[i], column_sets[[4]]$Mean4[i], column_sets[[4]]$SD4[i], column_sets[[4]]$N4[i]))
+  row <- cbind(row, calculate_g(column_sets[[5]]$Temp5[i], column_sets[[5]]$Mean5[i], column_sets[[5]]$SD5[i], column_sets[[5]]$N5[i]))
+  row <- cbind(row, calculate_g(column_sets[[6]]$Temp6[i], column_sets[[6]]$Mean6[i], column_sets[[6]]$SD6[i], column_sets[[6]]$N6[i]))
+  row <- cbind(row, calculate_g(column_sets[[7]]$Temp7[i], column_sets[[7]]$Mean7[i], column_sets[[7]]$SD7[i], column_sets[[7]]$N7[i]))
+  row <- cbind(row, calculate_g(column_sets[[8]]$Temp8[i], column_sets[[8]]$Mean8[i], column_sets[[8]]$SD8[i], column_sets[[8]]$N8[i]))
+  row <- cbind(row, calculate_g(column_sets[[9]]$Temp9[i], column_sets[[9]]$Mean9[i], column_sets[[9]]$SD9[i], column_sets[[9]]$N9[i]))
+
+  gdata <- rbind(gdata, row)
+  
+}
+### creating error when N=1 because of SMD formula. dont use for now. 
+
+gdata <- as.data.frame(gdata)
+
+new_column <- apply(gdata, 1, function(row) any(!is.na(row) & row < 3))
+gearys_test <- cbind(warmdata$Paper.code, gdata, new_column)
