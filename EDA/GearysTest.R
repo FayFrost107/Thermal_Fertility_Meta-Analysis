ddb519e60a6208dad5329c524bd2222a1ed43f22
FayFrost107 <fayfrost107@gmail.com>
2023-10-05 14:29:57 +0100

    Update GearysTest.R

--- EDA/GearysTest.R
+++ EDA/GearysTest.R
@@ -43,7 +43,6 @@ for(i in 1:nrow(warmdata)){
   gdata <- rbind(gdata, row)
   
 }
-### creating error when N=1 because of SMD formula. dont use for now. 
 
 gdata <- as.data.frame(gdata)
 
@@ -98,4 +97,56 @@ total_count <- sum(result)
   }
  
  outlier_temps <- as.data.frame(outlier_temps)
+ 
+ 
+ ############################### think i can combine this into the piarwise calc so I can easily remove specific es.
+ ref_temp <- warmdata$ref_temp
+ ref_mean <- warmdata$ref_mean
+ ref_sd <- warmdata$ref_sd
+ ref_ani <- warmdata$ref_ani
+ ref_N <- warmdata$ref_N
+ 
+ 
+ 
+ calculate_smd <- function(rtemp1, rmean1, rsd1, rani1, rn1, temp2, mean2, sd2, ani2, n2){
+   if (is.na(temp2)) {
+     smd <- NA  # Initialize as NA (numeric)
+     smd_v <- NA
+     gtest <- NA
+   } else if (rtemp1 != temp2) {
+     smd <- escalc(measure = "SMD", m2i=rmean1, sd2i=rsd1, n2i=rn1, m1i=mean2, sd1i=sd2, n1i=n2)[1]
+     smd_v <- escalc(measure = "SMD", m2i=rmean1, sd2i=rsd1, n2i=rani1, m1i=mean2, sd1i=sd2, n1i=ani2)[2]
+   } else {
+     smd <- 0  
+     smd_v <- 0 # rtemp1 equals temp2, so smd is set to 0
+   }
+   Smd <- cbind(smd, smd_v, temp2-rtemp1, rtemp1, temp2, calculate_g(temp2, mean2, sd2, n2))
+   return(Smd)
+ }
+ 
+ 
+ esdata <- c()
+ for(i in 1:nrow(warmdata)){
+   row <- calculate_smd(ref_temp[i], ref_mean[i], ref_sd[i], ref_ani[i], ref_N[i], column_sets[[1]]$Temp1[i], column_sets[[1]]$Mean1[i], column_sets[[1]]$SD1[i], column_sets[[1]]$ani_1[i], column_sets[[1]]$N1[i])
+   row <- cbind(row, calculate_smd(ref_temp[i], ref_mean[i], ref_sd[i], ref_ani[i], ref_N[i], column_sets[[2]]$Temp2[i], column_sets[[2]]$Mean2[i], column_sets[[2]]$SD2[i], column_sets[[2]]$ani_2[i], column_sets[[2]]$N2[i]))
+   row <- cbind(row, calculate_smd(ref_temp[i], ref_mean[i], ref_sd[i], ref_ani[i], ref_N[i], column_sets[[3]]$Temp3[i], column_sets[[3]]$Mean3[i], column_sets[[3]]$SD3[i], column_sets[[3]]$ani_3[i], column_sets[[3]]$N3[i]))
+   row <- cbind(row, calculate_smd(ref_temp[i], ref_mean[i], ref_sd[i], ref_ani[i], ref_N[i], column_sets[[4]]$Temp4[i], column_sets[[4]]$Mean4[i], column_sets[[4]]$SD4[i], column_sets[[4]]$ani_4[i], column_sets[[4]]$N4[i]))
+   row <- cbind(row, calculate_smd(ref_temp[i], ref_mean[i], ref_sd[i], ref_ani[i], ref_N[i], column_sets[[5]]$Temp5[i], column_sets[[5]]$Mean5[i], column_sets[[5]]$SD5[i], column_sets[[5]]$ani_5[i], column_sets[[5]]$N5[i]))
+   row <- cbind(row, calculate_smd(ref_temp[i], ref_mean[i], ref_sd[i], ref_ani[i], ref_N[i], column_sets[[6]]$Temp6[i], column_sets[[6]]$Mean6[i], column_sets[[6]]$SD6[i], column_sets[[6]]$ani_6[i], column_sets[[6]]$N6[i]))
+   row <- cbind(row, calculate_smd(ref_temp[i], ref_mean[i], ref_sd[i], ref_ani[i], ref_N[i], column_sets[[7]]$Temp7[i], column_sets[[7]]$Mean7[i], column_sets[[7]]$SD7[i], column_sets[[7]]$ani_7[i], column_sets[[7]]$N7[i]))
+   row <- cbind(row, calculate_smd(ref_temp[i], ref_mean[i], ref_sd[i], ref_ani[i], ref_N[i], column_sets[[8]]$Temp8[i], column_sets[[8]]$Mean8[i], column_sets[[8]]$SD8[i], column_sets[[8]]$ani_8[i], column_sets[[8]]$N8[i]))
+   row <- cbind(row, calculate_smd(ref_temp[i], ref_mean[i], ref_sd[i], ref_ani[i], ref_N[i], column_sets[[9]]$Temp9[i], column_sets[[9]]$Mean9[i], column_sets[[9]]$SD9[i], column_sets[[9]]$ani_9[i], column_sets[[9]]$N9[i]))
+   
+   colnames(row) <- c("es.1", "v.1", "??", "reftemp", "treattemp", "gtest",
+                      "es.2", "v.2", "??", "reftemp", "treattemp", "gtest", 
+                      "es.3", "v.3", "??", "reftemp", "treattemp", "gtest",
+                      "es.4", "v.4", "??", "reftemp", "treattemp", "gtest",
+                      "es.5", "v.5", "??", "reftemp", "treattemp", "gtest",
+                      "es.6", "v.6", "??", "reftemp", "treattemp", "gtest",
+                      "es.7", "v.7", "??", "reftemp", "treattemp", "gtest",
+                      "es.8", "v.8", "??", "reftemp", "treattemp", "gtest",
+                      "es.9", "v.9", "??", "reftemp", "treattemp", "gtest")
+   esdata <- rbind(esdata, row)
+   
+ }
  
